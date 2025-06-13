cad8ccc00d5a7aeb3114191d74e07f6a24b772aa
martin <martinpaush@gmail.co>
2025-06-13 21:30:53 +0200

    - name apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,10 +10,13 @@ android {
         applicationId = "io.celox.hexpulse"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // Set the APK name
+        setProperty("archivesBaseName", "HexPulse-${versionName}")
     }
 
     buildTypes {
