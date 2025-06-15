caf6f43326e0f5ebf719cb56d389f96a52190d15
martin <martinpaush@gmail.co>
2025-06-15 04:37:25 +0200

    - 0.0.6 (cross platform multiplayer working)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.celox.hexpulse"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
